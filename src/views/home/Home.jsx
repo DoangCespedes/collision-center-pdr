141e2f61a0ee7d59f4a5b03654a9340d20bcccb6
DoangCespedes <myemail@example.com>
2024-05-16 04:16:48 -0700

    cambio de imagenes

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
-import Parallax from '../../core/infrastructure/components/parallax/Parallax'
+import "./Styles.css"
+// import Parallax from '../../core/infrastructure/components/parallax/Parallax'
+import logo from "../../assets/logo3.png"
+import { BiSolidCarMechanic } from "react-icons/bi";
 
 export const Home = () => {
   return (
-    <Parallax/>
+    <>
+        <div>
+            <div className="bgimg-1">
+                <div className="caption">
+                        <img src={logo} style={{width:"35vw", color:"black"}}/>
+                </div>
+            </div>
+
+            <article className="section container" style={{ color:"#f5f5f5", background:"#202020"}}>
+                <div style={{width:"70%", textAlign:"center"}}>
+                    <h2><BiSolidCarMechanic/> Welcome to Collision Center & PDR !! <BiSolidCarMechanic/></h2>
+                    <p style={{fontSize:"1.3rem"}}>
+                        Your premier destination for auto repair in Colorado! With over 20 years of experience, we provide dent, collision, and hail damage repair services. Our highly skilled team ensures flawless results using state-of-the-art technology. We prioritize customer satisfaction and vehicle integrity. Explore our services online and contact us to schedule an appointment. Thank you for choosing Collision Center & PDR to restore your vehicle's beauty and performance!
+                    </p>
+                </div>
+            </article>
+
+            <div className="bgimg-2">
+                <div className="caption">
+                    <span className="border">Discover Our Premier Services</span>
+                </div>
+            </div>
+
+            <div className="section">
+                <h3 className="border"><b>Collision - Center & PDR</b></h3>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam labore quo similique nesciunt quas fugit magnam quis vitae debitis reiciendis.
+                    Illo, magni quam vitae suscipit sunt doloremque vero mollitia quisquam.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam labore quo similique nesciunt quas fugit magnam quis vitae debitis reiciendis.
+                    Illo, magni quam vitae suscipit sunt doloremque vero mollitia quisquam.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam labore quo similique nesciunt quas fugit magnam quis vitae debitis reiciendis.
+                    Illo, magni quam vitae suscipit sunt doloremque vero mollitia quisquam.
+                </p>
+            </div>
+
+            <div className="bgimg-3">
+                <div className="caption">
+                    <img src={logo} style={{width:"35vw", color:"black"}}/>
+                </div>
+            </div>
+
+
+        </div>
+    </>
   )
 }
